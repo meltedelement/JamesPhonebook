19e3d40238942477d21fb22950ed1ae273a7e17c
U3RhcnQ <petrpershin6@gmail.com>
2025-01-31 10:56:37 +0000

    Initial InteliJ

--- src/Phonebook.java
+++ src/Phonebook.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class Phonebook{
-    
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Ultimate phonebook!");
+        System.out.println("What would you like to do today ?");
+        System.out.println("Options: 1(add) 2(delete) 3(update) 4(search) 5(quit)");
+
+
+
+
+
+
+
+
+    }
+
+
 }
