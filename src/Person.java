0e8cc48cf9cfbce302345bccb7c5a55cd92837f0
Meltedelement <meltedelement@gmail.com>
2025-01-31 09:56:23 +0000

    initial commit. Yay!

---
+++ src/Person.java
@@ -0,0 +1,25 @@
+public class Person {
+    private String name;
+ 
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    
+    
+        public Person(String name){
+            this.name = name;
+        }
+    
+        public static void main(String[] args){
+        }
+    
+    
+
+    public String getName(){
+        return(this.name);
+    }
+}
+
+    
