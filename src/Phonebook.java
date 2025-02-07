a2231442ce4c18e0f4de8d228a114edcd16954bd
U3RhcnQ <petrpershin6@gmail.com>
2025-02-07 09:13:02 +0000

    Initial InteliJ

--- src/Phonebook.java
+++ src/Phonebook.java
@@ -1,23 +1,53 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Phonebook{
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to the Ultimate phonebook!");
         System.out.println("What would you like to do today ?");
-        System.out.println("Options: 1(add) 2(delete) 3(update) 4(search) 5(quit)");
 
 
+        int input;
+        do {
 
+            System.out.println("\nOptions: (1)add (2)delete (3)update (4)search) (5)quit");
+            System.out.print("Input: ");
 
+            try {
+                input = scanner.nextInt();
 
+            }catch (InputMismatchException e){
+                input = -1;
 
+            }
 
+            switch (input) {
+                case (1):
+
+                    break;
+                case (2):
+
+                    break;
+                case (3):
+
+                    break;
+                case (4):
+
+                    break;
+                case(5):
+                    System.out.println("Thanks for using the ultimate Phonebook");
+                    break;
+                default:
+                    System.out.println("Sorry your input was invalid would you like to try again ?");
+                    scanner.next(); // Clear any dangling input
+                    break;
+            }
+        } while (input != 5);
 
     }
-
 
 }
